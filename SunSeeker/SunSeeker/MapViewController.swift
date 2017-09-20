05062ba83e9d1d83be9d5ed216385e47b57cc088
mikemiksch <mikemiksch@gmail.com>
2017-09-20 14:47:29 -0700

    Weather icons displaying to reflect weather conditions.

--- SunSeeker/SunSeeker/MapViewController.swift
+++ SunSeeker/SunSeeker/MapViewController.swift
@@ -22,19 +22,19 @@ class MapViewController: UIViewController, MKMapViewDelegate {
     var cities = [City]() {
         didSet {
             addAnnotations()
-//            print(cities.count)
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.weatherMap.delegate = self
         API.shared.fetchData(callback: { (cities) in
             OperationQueue.main.addOperation {
                 self.cities = cities ?? []
             }
         })
         let center = CLLocationCoordinate2DMake(47.6062, -122.3321)
-        let span = MKCoordinateSpanMake(50, 50)
+        let span = MKCoordinateSpanMake(5, 5)
         let region = MKCoordinateRegionMake(center, span)
         weatherMap.setRegion(region, animated: true)
         
@@ -42,37 +42,34 @@ class MapViewController: UIViewController, MKMapViewDelegate {
     
     func addAnnotations() {
         for city in cities {
-//            print(city.name)
-//            print(city.description)
-//            print(city.distance)
             let location = city.twoDLocation
             let annotation = CustomPointAnnotation()
             annotation.coordinate = location
             annotation.title = city.name
             annotation.subtitle = city.description
             annotation.imageName = city.icon
-//            print(city.icon)
-//            annotation.image = UIImage(contentsOfFile: bundlePath!)!
             weatherMap.addAnnotation(annotation)
         }
     }
     
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        if !(annotation is MKPointAnnotation) {
-            print("Not a valid MKPointAnnotation registration")
+        if !(annotation is CustomPointAnnotation) {
+            print("Not a valid CustomPointAnnotation registration")
             return nil
         }
-        print("hi")
         
-        var annotationView = weatherMap.dequeueReusableAnnotationView(withIdentifier: "weatherIcon")
+        let identifier = "weatherIcon"
+        
+        var annotationView = weatherMap.dequeueReusableAnnotationView(withIdentifier: identifier)
         if annotationView == nil {
-            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "weatherIcon")
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            annotationView?.canShowCallout = true
         } else {
             annotationView!.annotation = annotation
         }
         
         let customAnnotation = annotation as! CustomPointAnnotation
-        annotationView!.image = UIImage(named: "\(customAnnotation.imageName).png")
+        annotationView!.image = UIImage(named: "\(customAnnotation.imageName!).png")
         
         return annotationView
     }
